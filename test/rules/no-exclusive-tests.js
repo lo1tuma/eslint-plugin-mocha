2d3bbe9c601d9052b853847d0f44a270baab07f7
Mathias Schreck <schreck.mathias@googlemail.com>
2014-08-28 20:20:16 +0200

    Rewrite tests using ESLintTester

--- test/rules/no-exclusive-tests.js
+++ test/rules/no-exclusive-tests.js
@@ -1,63 +1,39 @@
 'use strict';
 
-var expect = require('chai').expect,
-    noExlusiveTests = require('../../lib/rules/no-exclusive-tests');
+var linter = require('eslint').linter,
+    ESLintTester = require('eslint-tester'),
+    eslintTester = new ESLintTester(linter),
+    expectedErrorMessage = 'Unexpected exclusive mocha test.';
 
+eslintTester.addRuleTest('lib/rules/no-exclusive-tests', {
 
-function esLinter(ruleName, ruleFunction) {
-    var runConfiguration = { rules: {} },
-        esLint = require('eslint').linter;
+    valid: [
+        'describe()',
+        'it()',
+        'describe.skip()',
+        'it.skip()',
+        'var appliedOnly = describe.only; appliedOnly.apply(describe)',
+        'var calledOnly = it.only; calledOnly.call(it)',
+        'var computedOnly = "only"; describe[computedOnly]()'
+    ],
 
-    runConfiguration.rules[ruleName] = 1; // activate rule
-    esLint.defineRule(ruleName, ruleFunction);
-
-    return {
-        verify: function (javascriptCodeSnippet) {
-            return esLint.verify(javascriptCodeSnippet, runConfiguration);
+    invalid: [
+        {
+            code: 'describe.only()',
+            errors: [ { message: expectedErrorMessage } ]
+        },
+        {
+            code: 'describe["only"]()',
+            errors: [ { message: expectedErrorMessage } ]
+        },
+        {
+            code: 'it.only()',
+            errors: [ { message: expectedErrorMessage } ]
+        },
+        {
+            code: 'it["only"]()',
+            errors: [ { message: expectedErrorMessage } ]
         }
-    };
-}
-
-
-describe('Test ESLint no-exclusive-tests rule', function () {
-
-    var noExclusiveTestsRuleName = 'no-exclusive-tests',
-        noExlusiveTestsLinter = esLinter(noExclusiveTestsRuleName, noExlusiveTests),
-        ruleViolatingTestcases = [
-            'describe.only()',
-            'describe["only"]()',
-            'it.only()',
-            'it["only"]()'
-        ],
-        undetectedRuleViolationTestcases = [
-            'var appliedOnly = describe.only; appliedOnly.apply(describe)',
-            'var calledOnly = it.only; calledOnly.call(it)',
-            'var computedOnly = "only"; describe[computedOnly]()'
-        ];
-
-    ruleViolatingTestcases.forEach(function (testcase) {
-
-        it('should detect simple and static "' + testcase + '" calls', function () {
-            var errors = noExlusiveTestsLinter.verify(testcase),
-                firstError;
-
-            expect(errors).to.exist;
-            expect(errors).to.have.length(1);
-
-            firstError = errors[0];
-            expect(firstError.ruleId).to.equal(noExclusiveTestsRuleName);
-        });
-
-    });
-
-    undetectedRuleViolationTestcases.forEach(function (testcase) {
-
-        it('will fail to detect complex "' + testcase + '" calls', function () {
-            var errors = noExlusiveTestsLinter.verify(testcase);
-
-            expect(errors).to.be.empty;
-        });
-
-    });
+    ]
 
 });
