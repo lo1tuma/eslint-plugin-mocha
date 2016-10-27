c5c28f1e0c3c3b6e7d896eb9a9916e4682aedf5d
rhysd <lin90162@yahoo.co.jp>
2016-10-27 20:01:49 +0900

    DRY common errors
    Fix https://github.com/lo1tuma/eslint-plugin-mocha/pull/112#discussion_r85297707

--- test/rules/no-mocha-arrows.js
+++ test/rules/no-mocha-arrows.js
@@ -8,7 +8,8 @@ var RuleTester = require('eslint').RuleTester,
     expectedErrorMessage = 'Do not pass arrow functions to it()',
     es2017RuleTester = new RuleTester({
         parserOptions: { ecmaVersion: 2017 }
-    });
+    }),
+    errors = [ { message: expectedErrorMessage, column: 1, line: 1 } ];
 
 es6RuleTester.run('no-mocha-arrows', rules['no-mocha-arrows'], {
 
@@ -26,23 +27,23 @@ es6RuleTester.run('no-mocha-arrows', rules['no-mocha-arrows'], {
     invalid: [
         {
             code: 'it(() => { assert(something, false); })',
-            errors: [ { message: expectedErrorMessage, column: 1, line: 1 } ],
+            errors: errors,
             output: 'it(function () { assert(something, false); })'
         },
         {
             code: 'it(() => { assert(something, false); })',
             globals: [ 'it' ],
-            errors: [ { message: expectedErrorMessage, column: 1, line: 1 } ],
+            errors: errors,
             output: 'it(function () { assert(something, false); })'
         },
         {
             code: 'it(() => assert(something, false))',
-            errors: [ { message: expectedErrorMessage, column: 1, line: 1 } ],
+            errors: errors,
             output: 'it(function () { return assert(something, false); })'
         },
         {
             code: 'it("should be false", () => { assert(something, false); })',
-            errors: [ { message: expectedErrorMessage, column: 1, line: 1 } ],
+            errors: errors,
             output: 'it("should be false", function () { assert(something, false); })'
         },
         {
@@ -52,12 +53,12 @@ es6RuleTester.run('no-mocha-arrows', rules['no-mocha-arrows'], {
         },
         {
             code: 'it((done) => { assert(something, false); })',
-            errors: [ { message: expectedErrorMessage, column: 1, line: 1 } ],
+            errors: errors,
             output: 'it(function (done) { assert(something, false); })'
         },
         {
             code: 'it("should be false", () => {\n assert(something, false);\n})',
-            errors: [ { message: expectedErrorMessage, column: 1, line: 1 } ],
+            errors: errors,
             output: 'it("should be false", function () {\n assert(something, false);\n})'
         }
     ]
@@ -73,17 +74,17 @@ es2017RuleTester.run('no-mocha-arrows', rules['no-mocha-arrows'], {
     invalid: [
         {
             code: 'it(async () => { assert(something, false) })',
-            errors: [ { message: expectedErrorMessage, column: 1, line: 1 } ],
+            errors: errors,
             output: 'it(async function () { assert(something, false) })'
         },
         {
             code: 'it(async () => assert(something, false))',
-            errors: [ { message: expectedErrorMessage, column: 1, line: 1 } ],
+            errors: errors,
             output: 'it(async function () { return assert(something, false); })'
         },
         {
             code: 'it(async() => assert(something, false))',
-            errors: [ { message: expectedErrorMessage, column: 1, line: 1 } ],
+            errors: errors,
             output: 'it(async function() { return assert(something, false); })'
         }
     ]
