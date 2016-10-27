f132ec7c75882ec70a4314c68b1770991a216400
rhysd <lin90162@yahoo.co.jp>
2016-10-27 19:59:45 +0900

    Separate test runner by ecmaVersion
    Fix https://github.com/lo1tuma/eslint-plugin-mocha/pull/112#discussion_r85297128

--- test/rules/no-mocha-arrows.js
+++ test/rules/no-mocha-arrows.js
@@ -2,91 +2,87 @@
 
 var RuleTester = require('eslint').RuleTester,
     rules = require('../../').rules,
-    ruleTester = new RuleTester(),
-    expectedErrorMessage = 'Do not pass arrow functions to it()';
+    es6RuleTester = new RuleTester({
+        parserOptions: { ecmaVersion: 6 }
+    }),
+    expectedErrorMessage = 'Do not pass arrow functions to it()',
+    es2017RuleTester = new RuleTester({
+        parserOptions: { ecmaVersion: 2017 }
+    });
 
-ruleTester.run('no-mocha-arrows', rules['no-mocha-arrows'], {
+es6RuleTester.run('no-mocha-arrows', rules['no-mocha-arrows'], {
 
     valid: [
         'it()',
         'it(function() { assert(something, false); })',
         'it("should be false", function() { assert(something, false); })',
-        {
-            // In this example, `it` is not a global.
-            code: 'function it () {}; it(() => { console.log("okay") })',
-            parserOptions: { ecmaVersion: 6 }
-        },
+         // In this example, `it` is not a global.
+        'function it () {}; it(() => { console.log("okay") })',
         'it.only()',
         'it(function(done) { assert(something, false); done(); })',
-        {
-            code: 'it(function*() { assert(something, false) })',
-            parserOptions: { ecmaVersion: 6 }
-        },
-        {
-            code: 'it(async function () { assert(something, false) })',
-            parserOptions: { ecmaVersion: 2017 }
-        }
+        'it(function*() { assert(something, false) })'
     ],
 
     invalid: [
         {
             code: 'it(() => { assert(something, false); })',
-            parserOptions: { ecmaVersion: 6 },
             errors: [ { message: expectedErrorMessage, column: 1, line: 1 } ],
             output: 'it(function () { assert(something, false); })'
         },
         {
             code: 'it(() => { assert(something, false); })',
             globals: [ 'it' ],
-            parserOptions: { ecmaVersion: 6 },
             errors: [ { message: expectedErrorMessage, column: 1, line: 1 } ],
             output: 'it(function () { assert(something, false); })'
         },
         {
             code: 'it(() => assert(something, false))',
-            parserOptions: { ecmaVersion: 6 },
             errors: [ { message: expectedErrorMessage, column: 1, line: 1 } ],
             output: 'it(function () { return assert(something, false); })'
         },
         {
             code: 'it("should be false", () => { assert(something, false); })',
-            parserOptions: { ecmaVersion: 6 },
             errors: [ { message: expectedErrorMessage, column: 1, line: 1 } ],
             output: 'it("should be false", function () { assert(something, false); })'
         },
         {
             code: 'it.only(() => { assert(something, false); })',
-            parserOptions: { ecmaVersion: 6 },
             errors: [ { message: 'Do not pass arrow functions to it.only()', column: 1, line: 1 } ],
             output: 'it.only(function () { assert(something, false); })'
         },
         {
             code: 'it((done) => { assert(something, false); })',
-            parserOptions: { ecmaVersion: 6 },
             errors: [ { message: expectedErrorMessage, column: 1, line: 1 } ],
             output: 'it(function (done) { assert(something, false); })'
         },
         {
             code: 'it("should be false", () => {\n assert(something, false);\n})',
-            parserOptions: { ecmaVersion: 6 },
             errors: [ { message: expectedErrorMessage, column: 1, line: 1 } ],
             output: 'it("should be false", function () {\n assert(something, false);\n})'
-        },
+        }
+    ]
+
+});
+
+es2017RuleTester.run('no-mocha-arrows', rules['no-mocha-arrows'], {
+
+    valid: [
+        'it(async function () { assert(something, false) })'
+    ],
+
+    invalid: [
         {
             code: 'it(async () => { assert(something, false) })',
-            parserOptions: { ecmaVersion: 2017 },
             errors: [ { message: expectedErrorMessage, column: 1, line: 1 } ],
             output: 'it(async function () { assert(something, false) })'
         },
         {
             code: 'it(async () => assert(something, false))',
-            parserOptions: { ecmaVersion: 2017 },
             errors: [ { message: expectedErrorMessage, column: 1, line: 1 } ],
             output: 'it(async function () { return assert(something, false); })'
         },
         {
             code: 'it(async() => assert(something, false))',
-            parserOptions: { ecmaVersion: 2017 },
             errors: [ { message: expectedErrorMessage, column: 1, line: 1 } ],
             output: 'it(async function() { return assert(something, false); })'
         }
