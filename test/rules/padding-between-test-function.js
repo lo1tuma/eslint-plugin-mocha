d1134432497836c2ecc4dc559000a158ed9261d4
Tyler Waters <tyler.waters@gmail.com>
2017-11-05 19:01:06 -0800

    More test cases for padding-between-test-functions

--- test/rules/padding-between-test-function.js
+++ test/rules/padding-between-test-function.js
@@ -11,38 +11,174 @@ ruleTester.run('padding-between-tests', rules['padding-between-test-functions'],
 
     valid: [
         {
-            code: 'describe(function(){\n\nit("", function () {})\n\n})'
+            code: [
+                'describe(function(){',
+                '',
+                'it("", function () {})',
+                '',
+                '})'
+            ].join('\n')
         },
         {
-            code: 'describe(function(){\n\nit("", function () {})\n\n})',
+            code: [
+                'describe(function(){',
+                '',
+                'it("", function () {})',
+                '',
+                '})'
+            ].join('\n'),
             options: [ 'always' ]
         },
         {
-            code: 'describe(function(){\n\nit.only("", function () {})\n\n})',
+            code: [
+                'describe(function(){',
+                '',
+                'it.only("", function () {})',
+                '',
+                '})'
+            ].join('\n'),
             options: [ 'always' ]
         },
         {
-            code: 'describe(function(){\nit("", function () {})\n})',
+            code: [
+                'describe(function(){',
+                'it("", function () {})',
+                '})'
+            ].join('\n'),
             options: [ 'never' ]
         },
         {
-            code: 'describe(function(){\nit.only("", function () {})\n})',
+            code: [
+                'describe(function(){',
+                'it.only("", function () {})',
+                '})'
+            ].join('\n'),
             options: [ 'never' ]
+        },
+        {
+            code: [
+                '[1,2,3].forEach(function () {',
+                'it("foo", function () {});',
+                '});'
+            ].join('\n'),
+            options: [ 'never' ]
+        },
+        {
+            code: [
+                '[1,2,3].forEach(function () {',
+                '',
+                'it("foo", function () {});',
+                '',
+                '});'
+            ].join('\n'),
+            options: [ 'always' ]
+        },
+        {
+            code: [
+                'describe("foo", {});',
+                'somethingElse();'
+            ].join('\n'),
+            options: [ 'never' ]
+        },
+        {
+            code: [
+                'describe("foo", {});',
+                '',
+                'somethingElse();'
+            ].join('\n'),
+            options: [ 'always' ]
         }
     ],
 
     invalid: [
         {
-            code: 'describe(function(){\n\nit("", function () {})\n\n})',
+            code: [
+                'describe(function(){',
+                '',
+                'it("", function () {})',
+                '',
+                '})'
+            ].join('\n'),
             options: [ 'never' ],
             errors: [ { message: NEVER_MESSAGE }, { message: NEVER_MESSAGE } ],
-            output: 'describe(function(){\nit("", function () {})\n})'
+            output: [
+                'describe(function(){',
+                'it("", function () {})',
+                '})'
+            ].join('\n')
         },
         {
-            code: 'describe(function(){\nit("", function () {})\n})',
+            code: [
+                'describe(function(){',
+                'it("", function () {})',
+                '})'
+            ].join('\n'),
             options: [ 'always' ],
             errors: [ { message: ALWAYS_MESSAGE }, { message: ALWAYS_MESSAGE } ],
-            output: 'describe(function(){\n\nit("", function () {})\n\n})'
+            output: [
+                'describe(function(){',
+                '',
+                'it("", function () {})',
+                '',
+                '})'
+            ].join('\n')
+        },
+        {
+            code: [
+                '[1,2,3].forEach(function () {',
+                '',
+                'it("foo", function () {});',
+                '',
+                '});'
+            ].join('\n'),
+            options: [ 'never' ],
+            errors: [ { message: NEVER_MESSAGE }, { message: NEVER_MESSAGE } ],
+            output: [
+                '[1,2,3].forEach(function () {',
+                'it("foo", function () {});',
+                '});'
+            ].join('\n')
+        },
+        {
+            code: [
+                '[1,2,3].forEach(function () {',
+                'it("foo", function () {});',
+                '});'
+            ].join('\n'),
+            options: [ 'always' ],
+            errors: [ { message: ALWAYS_MESSAGE }, { message: ALWAYS_MESSAGE } ],
+            output: [
+                '[1,2,3].forEach(function () {',
+                '',
+                'it("foo", function () {});',
+                '',
+                '});'
+            ].join('\n')
+        },
+        {
+            code: [
+                'describe(function(){',
+                'var someVar=null;',
+                'beforeEach(function(){someVar = "test"});',
+                'it("foo", function(){assert(someVar)});',
+                '})'
+            ].join('\n'),
+            options: [ 'always' ],
+            errors: [
+                { message: ALWAYS_MESSAGE },
+                { message: ALWAYS_MESSAGE },
+                { message: ALWAYS_MESSAGE }
+            ],
+            output: [
+                'describe(function(){',
+                'var someVar=null;',
+                '',
+                'beforeEach(function(){someVar = "test"});',
+                '',
+                'it("foo", function(){assert(someVar)});',
+                '',
+                '})'
+            ].join('\n')
         }
     ]
 
