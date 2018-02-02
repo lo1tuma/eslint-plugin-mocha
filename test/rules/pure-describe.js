1c995732199643d72d3180b783bd30b5d6fa96e0
Don Abrams <donabrams@invisionapp.com>
2018-02-02 07:44:50 -0700

    add member detection (due to getters/setters) and restrict nesting to only activate in describe blocks

--- test/rules/pure-describe.js
+++ test/rules/pure-describe.js
@@ -8,10 +8,19 @@ ruleTester.run('pure-describe', rule, {
     valid: [
         'it()',
         'it(); it(); it()',
+        'a.b',
+        'b()',
+        'function g() { a() }',
+        { code: '() => { a.b }', parserOptions: { ecmaVersion: 6 } },
+        'it("", function () { b(); })',
+        'it("", function () { a.b; })',
         'describe("", function () { it(); })',
         'describe("", function () { it("", function () { b(); }); })',
-        'describe("", function () { function a() { b() }; it(); })',
+        'describe("", function () { it("", function () { a.b; }); })',
+        'describe("", function () { function a() { b(); }; it(); })',
+        'describe("", function () { function a() { b.c; }; it(); })',
         { code: 'describe("", function () { var a = () => b(); it(); })', parserOptions: { ecmaVersion: 6 } },
+        { code: 'describe("", function () { var a = () => b.c; it(); })', parserOptions: { ecmaVersion: 6 } },
         'describe("", function () { describe("", function () { it(); }); it(); })',
         {
             code: 'foo("", function () { it(); })',
@@ -25,16 +34,55 @@ ruleTester.run('pure-describe', rule, {
                    additionalSuiteNames: [ 'foo' ]
                }
            }
+        }, {
+            code: 'foo("", function () { it("", function () { b(); }); })',
+            settings: {
+                mocha: {
+                   additionalSuiteNames: [ 'foo' ]
+               }
+           }
         }
     ],
 
     invalid: [
         {
-            code: 'describe("", function () { a() });',
+            code: 'describe("", function () { a(); });',
             errors: [ {
-                message: 'Unexpected function call inside describe.',
+                message: 'Unexpected function call in describe block.',
                 line: 1,
                 column: 28
+            } ]
+        }, {
+            code: 'foo("", function () { a(); });',
+            settings: {
+                mocha: {
+                   additionalSuiteNames: [ 'foo' ]
+               }
+            },
+            errors: [ {
+                message: 'Unexpected function call in describe block.',
+                line: 1,
+                column: 23
+            } ]
+        },
+        {
+            code: 'describe("", function () { a.b; });',
+            errors: [ {
+                message: 'Unexpected dot operator in describe block.',
+                line: 1,
+                column: 28
+            } ]
+        }, {
+            code: 'foo("", function () { a.b; });',
+            settings: {
+                mocha: {
+                   additionalSuiteNames: [ 'foo' ]
+               }
+            },
+            errors: [ {
+                message: 'Unexpected dot operator in describe block.',
+                line: 1,
+                column: 23
             } ]
         }
     ]
