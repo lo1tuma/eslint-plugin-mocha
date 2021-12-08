ea649d04895e43d9682dba7e0da5c5cc4b1bfbfb
Bryan Mishkin <bmishkin@squareup.com>
2021-12-08 16:26:50 -0500

    chore: use requireindex to export all rules

--- index.js
+++ index.js
@@ -1,31 +1,9 @@
 'use strict';
 
+const requireIndex = require('requireindex');
+
 module.exports = {
-    rules: {
-        'handle-done-callback': require('./lib/rules/handle-done-callback'),
-        'max-top-level-suites': require('./lib/rules/max-top-level-suites'),
-        'no-async-describe': require('./lib/rules/no-async-describe'),
-        'no-exclusive-tests': require('./lib/rules/no-exclusive-tests'),
-        'no-exports': require('./lib/rules/no-exports'),
-        'no-global-tests': require('./lib/rules/no-global-tests'),
-        'no-hooks': require('./lib/rules/no-hooks'),
-        'no-hooks-for-single-case': require('./lib/rules/no-hooks-for-single-case'),
-        'no-identical-title': require('./lib/rules/no-identical-title'),
-        'no-mocha-arrows': require('./lib/rules/no-mocha-arrows'),
-        'no-nested-tests': require('./lib/rules/no-nested-tests'),
-        'no-pending-tests': require('./lib/rules/no-pending-tests'),
-        'no-return-and-callback': require('./lib/rules/no-return-and-callback'),
-        'no-return-from-async': require('./lib/rules/no-return-from-async'),
-        'no-setup-in-describe': require('./lib/rules/no-setup-in-describe'),
-        'no-sibling-hooks': require('./lib/rules/no-sibling-hooks'),
-        'no-skipped-tests': require('./lib/rules/no-skipped-tests'),
-        'no-synchronous-tests': require('./lib/rules/no-synchronous-tests'),
-        'no-top-level-hooks': require('./lib/rules/no-top-level-hooks'),
-        'prefer-arrow-callback': require('./lib/rules/prefer-arrow-callback'),
-        'valid-suite-description': require('./lib/rules/valid-suite-description'),
-        'valid-test-description': require('./lib/rules/valid-test-description'),
-        'no-empty-description': require('./lib/rules/no-empty-description.js')
-    },
+    rules: requireIndex(`${__dirname}/lib/rules`),
     configs: {
         all: {
             env: { mocha: true },
