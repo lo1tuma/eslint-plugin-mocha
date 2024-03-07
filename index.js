6c65ca6fbc22348744c3795124677ba1b60b6e5f
rhysd <lin90162@yahoo.co.jp>
2024-03-07 15:28:25 +0900

    Add support for eslint flat config format

--- index.js
+++ index.js
@@ -1,6 +1,62 @@
 'use strict';
 
-module.exports = {
+const globals = require('globals');
+
+const allRules = {
+    'mocha/handle-done-callback': 'error',
+    'mocha/max-top-level-suites': 'error',
+    'mocha/no-async-describe': 'error',
+    'mocha/no-exclusive-tests': 'error',
+    'mocha/no-exports': 'error',
+    'mocha/no-global-tests': 'error',
+    'mocha/no-hooks': 'error',
+    'mocha/no-hooks-for-single-case': 'error',
+    'mocha/no-identical-title': 'error',
+    'mocha/no-mocha-arrows': 'error',
+    'mocha/no-nested-tests': 'error',
+    'mocha/no-pending-tests': 'error',
+    'mocha/no-return-and-callback': 'error',
+    'mocha/no-return-from-async': 'error',
+    'mocha/no-setup-in-describe': 'error',
+    'mocha/no-sibling-hooks': 'error',
+    'mocha/no-skipped-tests': 'error',
+    'mocha/no-synchronous-tests': 'error',
+    'mocha/no-top-level-hooks': 'error',
+    'mocha/prefer-arrow-callback': 'error',
+    'mocha/valid-suite-description': 'error',
+    'mocha/valid-test-description': 'error',
+    'mocha/no-empty-description': 'error',
+    'mocha/consistent-spacing-between-blocks': 'error'
+};
+
+const recommendedRules = {
+    'mocha/handle-done-callback': 'error',
+    'mocha/max-top-level-suites': [ 'error', { limit: 1 } ],
+    'mocha/no-async-describe': 'error',
+    'mocha/no-exclusive-tests': 'warn',
+    'mocha/no-exports': 'error',
+    'mocha/no-global-tests': 'error',
+    'mocha/no-hooks': 'off',
+    'mocha/no-hooks-for-single-case': 'off',
+    'mocha/no-identical-title': 'error',
+    'mocha/no-mocha-arrows': 'error',
+    'mocha/no-nested-tests': 'error',
+    'mocha/no-pending-tests': 'warn',
+    'mocha/no-return-and-callback': 'error',
+    'mocha/no-return-from-async': 'off',
+    'mocha/no-setup-in-describe': 'error',
+    'mocha/no-sibling-hooks': 'error',
+    'mocha/no-skipped-tests': 'warn',
+    'mocha/no-synchronous-tests': 'off',
+    'mocha/no-top-level-hooks': 'warn',
+    'mocha/prefer-arrow-callback': 'off',
+    'mocha/valid-suite-description': 'off',
+    'mocha/valid-test-description': 'off',
+    'mocha/no-empty-description': 'error',
+    'mocha/consistent-spacing-between-blocks': 'error'
+};
+
+const mod = {
     rules: {
         'handle-done-callback': require('./lib/rules/handle-done-callback'),
         'max-top-level-suites': require('./lib/rules/max-top-level-suites'),
@@ -31,63 +87,27 @@ module.exports = {
         all: {
             env: { mocha: true },
             plugins: [ 'mocha' ],
-            rules: {
-                'mocha/handle-done-callback': 'error',
-                'mocha/max-top-level-suites': 'error',
-                'mocha/no-async-describe': 'error',
-                'mocha/no-exclusive-tests': 'error',
-                'mocha/no-exports': 'error',
-                'mocha/no-global-tests': 'error',
-                'mocha/no-hooks': 'error',
-                'mocha/no-hooks-for-single-case': 'error',
-                'mocha/no-identical-title': 'error',
-                'mocha/no-mocha-arrows': 'error',
-                'mocha/no-nested-tests': 'error',
-                'mocha/no-pending-tests': 'error',
-                'mocha/no-return-and-callback': 'error',
-                'mocha/no-return-from-async': 'error',
-                'mocha/no-setup-in-describe': 'error',
-                'mocha/no-sibling-hooks': 'error',
-                'mocha/no-skipped-tests': 'error',
-                'mocha/no-synchronous-tests': 'error',
-                'mocha/no-top-level-hooks': 'error',
-                'mocha/prefer-arrow-callback': 'error',
-                'mocha/valid-suite-description': 'error',
-                'mocha/valid-test-description': 'error',
-                'mocha/no-empty-description': 'error',
-                'mocha/consistent-spacing-between-blocks': 'error'
-            }
+            rules: allRules
         },
-
         recommended: {
             env: { mocha: true },
             plugins: [ 'mocha' ],
-            rules: {
-                'mocha/handle-done-callback': 'error',
-                'mocha/max-top-level-suites': [ 'error', { limit: 1 } ],
-                'mocha/no-async-describe': 'error',
-                'mocha/no-exclusive-tests': 'warn',
-                'mocha/no-exports': 'error',
-                'mocha/no-global-tests': 'error',
-                'mocha/no-hooks': 'off',
-                'mocha/no-hooks-for-single-case': 'off',
-                'mocha/no-identical-title': 'error',
-                'mocha/no-mocha-arrows': 'error',
-                'mocha/no-nested-tests': 'error',
-                'mocha/no-pending-tests': 'warn',
-                'mocha/no-return-and-callback': 'error',
-                'mocha/no-return-from-async': 'off',
-                'mocha/no-setup-in-describe': 'error',
-                'mocha/no-sibling-hooks': 'error',
-                'mocha/no-skipped-tests': 'warn',
-                'mocha/no-synchronous-tests': 'off',
-                'mocha/no-top-level-hooks': 'warn',
-                'mocha/prefer-arrow-callback': 'off',
-                'mocha/valid-suite-description': 'off',
-                'mocha/valid-test-description': 'off',
-                'mocha/no-empty-description': 'error',
-                'mocha/consistent-spacing-between-blocks': 'error'
-            }
+            rules: recommendedRules
         }
     }
 };
+
+mod.configs.flat = {
+    all: {
+        plugins: { mocha: mod },
+        languageOptions: { globals: globals.mocha },
+        rules: allRules
+    },
+    recommended: {
+        plugins: { mocha: mod },
+        languageOptions: { globals: globals.mocha },
+        rules: recommendedRules
+    }
+};
+
+module.exports = mod;
