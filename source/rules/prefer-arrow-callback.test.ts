6f4218f8fe052affe734f51511aba00a6146a8ce
Mathias Schreck <schreck.mathias@gmail.com>
2026-05-19 13:24:42 +0200

    Reduce prefer-arrow-callback integration tests

--- source/rules/prefer-arrow-callback.test.ts
+++ source/rules/prefer-arrow-callback.test.ts
@@ -26,22 +26,10 @@ const errors: [RuleTester.TestCaseError] = [{
 
 ruleTester.run('prefer-arrow-callback', preferArrowCallbackRule, {
     valid: [
+        // Smoke tests for the core ESLint rule integration.
         'foo(a => a);',
         'foo(function*() {});',
-        'foo(function() { this; });',
         { code: 'foo(function bar() {});', options: [{ allowNamedFunctions: true }] },
-        'foo(function() { (() => this); });',
-        'foo(function() { this; }.bind(obj));',
-        'foo(function() { this; }.call(this));',
-        'foo(a => { (function() {}); });',
-        'var foo = function foo() {};',
-        '(function foo() {})();',
-        'foo(function bar() { bar; });',
-        'foo(function bar() { arguments; });',
-        'foo(function bar() { arguments; }.bind(this));',
-        'foo(function bar() { new.target; });',
-        'foo(function bar() { new.target; }.bind(this));',
-        'foo(function bar() { this; }.bind(this, somethingElse));',
         {
             code: 'import.meta.url',
             languageOptions: {
@@ -78,71 +66,15 @@ ruleTester.run('prefer-arrow-callback', preferArrowCallbackRule, {
         'xspecify("name", function bar() {});'
     ],
     invalid: [
-        {
-            code: 'foo(function bar() {});',
-            output: 'foo(() => {});',
-            errors
-        },
+        // Smoke tests for the core ESLint rule integration.
         {
             code: 'foo(function() {});',
             output: 'foo(() => {});',
-            options: [{ allowNamedFunctions: true }],
-            errors
-        },
-        {
-            code: 'foo(function bar() {});',
-            output: 'foo(() => {});',
-            options: [{ allowNamedFunctions: false }],
-            errors
-        },
-        {
-            code: 'foo(function() {});',
-            output: 'foo(() => {});',
-            errors
-        },
-        {
-            code: 'foo(nativeCb || function() {});',
-            output: 'foo(nativeCb || (() => {}));',
-            errors
-        },
-        {
-            code: 'foo(bar ? function() {} : function() {});',
-            output: 'foo(bar ? () => {} : () => {});',
-            errors: [errors[0], errors[0]]
-        },
-        {
-            code: 'foo(function() { (function() { this; }); });',
-            output: 'foo(() => { (function() { this; }); });',
-            errors
-        },
-        {
-            code: 'foo(function() { this; }.bind(this));',
-            output: 'foo(() => { this; });',
             errors
         },
         {
             code: 'foo(bar || function() { this; }.bind(this));',
             output: 'foo(bar || (() => { this; }));',
-            errors
-        },
-        {
-            code: 'foo(function() { (() => this); }.bind(this));',
-            output: 'foo(() => { (() => this); });',
-            errors
-        },
-        {
-            code: 'foo(function bar(a) { a; });',
-            output: 'foo((a) => { a; });',
-            errors
-        },
-        {
-            code: 'foo(function(a) { a; });',
-            output: 'foo((a) => { a; });',
-            errors
-        },
-        {
-            code: 'foo(function(arguments) { arguments; });',
-            output: 'foo((arguments) => { arguments; });',
             errors
         },
         {
@@ -153,60 +85,12 @@ ruleTester.run('prefer-arrow-callback', preferArrowCallbackRule, {
             errors
         },
         {
-            code: 'foo(function() { (() => this); });',
-            // No fix applied
-            output: null,
-            options: [{ allowUnboundThis: false }],
-            errors
-        },
-        {
-            code: 'qux(function(foo, bar, baz) { return foo * 2; })',
-            output: 'qux((foo, bar, baz) => { return foo * 2; })',
-            errors
-        },
-        {
-            code: 'qux(function(foo, bar, baz) { return foo * bar; }.bind(this))',
-            output: 'qux((foo, bar, baz) => { return foo * bar; })',
-            errors
-        },
-        {
-            code: 'foo(function() {}.bind(this, somethingElse))',
-            output: 'foo((() => {}).bind(this, somethingElse))',
-            errors
-        },
-        {
-            code: "qux(function(foo = 1, [bar = 2] = [], {qux: baz = 3} = {foo: 'bar'}) { return foo + bar; });",
-            output: "qux((foo = 1, [bar = 2] = [], {qux: baz = 3} = {foo: 'bar'}) => { return foo + bar; });",
-            errors
-        },
-        {
-            code: 'qux(function(baz, baz) { })',
-            // Duplicate parameter names are a SyntaxError in arrow functions
-            output: null,
-            errors
-        },
-        {
-            code: 'qux(function( /* no params */ ) { })',
-            output: 'qux(( /* no params */ ) => { })',
-            errors
-        },
-        {
-            code: 'qux(function( /* a */ foo /* b */ , /* c */ bar /* d */ , /* e */ baz /* f */ ) { return foo; })',
-            output: 'qux(( /* a */ foo /* b */ , /* c */ bar /* d */ , /* e */ baz /* f */ ) => { return foo; })',
-            errors
-        },
-        {
             code: 'qux(async function (foo = 1, bar = 2, baz = 3) { return baz; })',
             output: 'qux(async (foo = 1, bar = 2, baz = 3) => { return baz; })',
             languageOptions: { ecmaVersion: 8 },
             errors
         },
-        {
-            code: 'qux(async function (foo = 1, bar = 2, baz = 3) { return this; }.bind(this))',
-            output: 'qux(async (foo = 1, bar = 2, baz = 3) => { return this; })',
-            languageOptions: { ecmaVersion: 8 },
-            errors
-        },
+        // Mocha-specific behavior.
         {
             code: 'it("name", function() { foo(function() {}); });',
             output: 'it("name", function() { foo(() => {}); });',
